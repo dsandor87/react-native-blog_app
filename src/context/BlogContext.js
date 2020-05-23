ac9ff94517356aaded0299a0d5aacc632853a18c
Sandor Desi <d.alex.galaxy@gmail.com>
2020-05-23 14:50:57 +0100

    Rendering a List of Posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,9 +4,17 @@ import React from 'react'
 const BlogContext = React.createContext()
 
 
-export const BlogProvider = ({ children }
-) => {
-  return <BlogContext.Provider value={123345676554313245}>
+export const BlogProvider = ({ children }) => {
+
+
+  const blogPosts = [
+    {
+      title: 'Blog Post #1'
+    }, {
+      title: 'Blog Post #2'
+    }
+  ]
+  return <BlogContext.Provider value={blogPosts}>
     {children}
 
 
