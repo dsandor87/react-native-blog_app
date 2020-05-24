a1ecb47c58a1cde55cc387eb6ff8c241a97ba348
Sandor Desi <d.alex.galaxy@gmail.com>
2020-05-24 10:48:17 +0100

    Automatic Context Creation

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,6 @@
 import React, { useReducer } from 'react'
+import createDataContext from './createDataContext'
 
-
-const BlogContext = React.createContext()
 
 const blogReducer = (state, action) => {
 
@@ -12,31 +11,16 @@ const blogReducer = (state, action) => {
       return state
   }
   
+  
 }
 
-
-
-export const BlogProvider = ({ children }) => {
-
-  const [blogPosts, dispatch] = useReducer(blogReducer, [])
-
-  const addBlogPost = () => {
+const addBlogPost = (dispatch) => {
+  return ()=> {
     dispatch({ type: 'add_blogpost' })
-  
   }
- 
-  
-  
-  
-
-
-  return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-    {children}
-
-
-  </BlogContext.Provider>
-  
 }
 
-
-export default BlogContext
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  [])
