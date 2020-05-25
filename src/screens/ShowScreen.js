7dd57932bb59332ce58454fb1950dec4360ed2a2
Sandor Desi <d.alex.galaxy@gmail.com>
2020-05-25 11:21:36 +0100

    The Edit Icon Link

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { Context } from '../context/BlogContext'
+import { FontAwesome5 } from '@expo/vector-icons'
 
 const ShowScreen = ({ navigation }) => {
   // console.log(navigation.getParam('id'))
@@ -14,8 +15,19 @@ const ShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   )
+}
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: (
+      <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+        <FontAwesome5 name='pen-alt' size={35} color='black' />
+      </TouchableOpacity>
+    )
+  }
 }
 
 const styles = StyleSheet.create({})
