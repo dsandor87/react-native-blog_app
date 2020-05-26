5b9465941a383356f07cf288a4eafe05e7743d74
Sandor Desi <d.alex.galaxy@gmail.com>
2020-05-26 11:59:38 +0100

    Editing Action Function and Reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,6 +3,10 @@ import createDataContext from './createDataContext'
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost
+      })
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload)
     case 'add_blogpost':
@@ -31,9 +35,14 @@ const deleteBlogPost = (dispatch) => {
     dispatch({ type: 'delete_blogpost', payload: id })
   }
 }
+const editBlogPost = (dispatch) => {
+  return (id, title, content) => {
+    dispatch({ type: 'edit_blogpost', payload: { id, content, title } })
+  }
+}
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   [{ title: 'Test', content: 'Test Contetn lorem ipsum', id: 1 }]
 )
